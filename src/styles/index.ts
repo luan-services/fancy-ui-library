dc78e20b1a7df74ba706eec07d806300667a0764
luan-services <luandotservices@gmail.com>
2025-11-17 00:07:23 -0300

    feat: added token and primitive colors to easy customize components

---
+++ src/styles/index.ts
@@ -0,0 +1,6 @@
+import "./tokens/fc-primitives.css";
+import "./tokens/fc-tokens.css";
+import "./themes/fc-theme-light.css";
+import "./themes/fc-theme-dark.css";
+
+export const themes = ["light", "dark"];
