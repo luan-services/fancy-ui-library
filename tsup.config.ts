d580c836d983fa2efe3c4657bfb1ac553f25021c
luan-services <luandotservices@gmail.com>
2025-12-03 22:16:07 -0300

    feat: added terser for removing comments on bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
   splitting: true,         // allows splitting the code
   treeshake: true,         // removes unused code (if you dont use defineOption() for example, it will be removed from final bundle)
   clean: true,
+  minify: 'terser',
+  terserOptions: { // configure terser to only remove comments
+    compress: false, // don't simplify code logic
+    mangle: false,   // don't rename variables
+    format: {
+      comments: false, // remove all comments
+      beautify: true,  
+    },
+  },
 });
 
 // this extracts all CSS imported in src/index.ts into a single dist/index.css there is no need to use --inject-style
